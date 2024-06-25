1ee248f23a6926c9c06f906d9ee6907c5e8de303
rafaelywata <rafaelywata@gmail.com>
2024-06-25 18:49:29 -0300

    - Alteradas as configurações de inicialização do hibernate; - Adicionadas as variáveis de ambiente nos perfis de PRD e DEV;

---
+++ src/main/java/me/dio/santander_java_backend_2024/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package me.dio.santander_java_backend_2024.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/santander_java_backend_2024/domain/model/News.java
@@ -0,0 +1,8 @@
+package me.dio.santander_java_backend_2024.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
